bd7bafee7c5b40580e5029e9521bda9cf50ca282
Ashis Kumar <srimanashis@gmail.com>
2021-06-22 23:19:42 +0530

    google

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
-const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
 
+app.get('/', async (req, res) => {
+    const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+    } : {
+        args: [],
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    });
 
-app.get('/', (req, res) => { res.send('Home Page Route') });
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 400,
+        height: 400,
+        deviceScaleFactor: 1
+    });
+    await page.goto('https://google.com');
+
+    let allHtml = await page.content();
+    // await page.setContent('<h1>Hello World!</h1>', { waitUntil: 'networkidle2' });
+    // await page.screenshot({ path: 'public/image.png' });
+    await browser.close();
+    res.status(200).json({ result: allHtml })
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+let server = app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+server.setTimeout(0)
+process.setMaxListeners(0);
