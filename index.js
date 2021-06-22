c1538cb6906263bab378e00ecd0cdcc2664181c1
Ashis Kumar <srimanashis@gmail.com>
2021-06-22 23:12:32 +0530

    google

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+const cheerio = require('cheerio');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', async (req, res) => {
+    const browser = await puppeteer.launch(process.env.AWS_EXECUTION_ENV ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless
+    } : {
+        args: [],
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+    });
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+    const page = await browser.newPage();
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+    await page.setViewport({
+        width: 400,
+        height: 400,
+        deviceScaleFactor: 1
+    });
+    await page.goto('https://google.com');
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+    let allHtml = await page.content();
+    let $ = cheerio.load(allHtml);
+    // await page.setContent('<h1>Hello World!</h1>', { waitUntil: 'networkidle2' });
+    // await page.screenshot({ path: 'public/image.png' });
+    await browser.close();
+    res.status(200).json({ result: $ })
+});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+let server = app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+server.setTimeout(0)
+process.setMaxListeners(0);
